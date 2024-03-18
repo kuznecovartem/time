8d552867113c085808d1d4edfd6f9bfef29feb61
kuznecovartem <122590171+kuznecovartem@users.noreply.github.com>
2024-03-18 16:42:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+const url1 = 'http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-019.jpg';
+const url2 = 'http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-023.jpg';
+const url3 = 'http://wallpapers-image.ru/2560x1600/cars/wallpapers/wallpapers-cars-09.jpg';
+
+const img = document.createElement('img');
+img.height = 200;
+img.src = url1;
+document.body.append(img);
+
+img.addEventListener('load', () => {
+    const img2 = document.createElement('img');
+    img2.height = 200;
+    img2.src = url2;
+    document.body.append(img2);
+
+    img2.addEventListener('load', () => {
+        const img3 = document.createElement('img');
+        img3.height = 200;
+        img3.src = url3;
+        document.body.append(img3);
+
+        img3.addEventListener('load', ()=> {
+            console.log('все картинки загруженны');
+        })
+    })
+})
+
